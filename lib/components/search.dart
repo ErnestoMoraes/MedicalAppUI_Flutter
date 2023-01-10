55857791b73cdfd759a1356c2076f86b1adc252d
ErnestoMoraes <joseernestosantosmoraes@gmail.com>
2023-01-10 14:38:43 -0300

    UI inicial

--- lib/components/search.dart
+++ lib/components/search.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:medicalappui/pages/homepage.dart';
-import 'package:medicalappui/pages/secondpage.dart';
 
 class MySearch extends StatelessWidget {
   final controller;
